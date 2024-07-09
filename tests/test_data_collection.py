d1520e1c4468009a9e28dcdfa4ac6134cb10b3fb
Your Name <you@example.com>
2024-07-08 20:30:51 -0700

    Updated test_data_collection

--- tests/test_data_collection.py
+++ tests/test_data_collection.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch, MagicMock
-from ..src.data_collection import changes_scraper, jira_scraper, cep_scraper, mailing_list_scraper
+from src.data_collection import changes_scraper, jira_scraper, cep_scraper, mailing_list_scraper
 
 class TestDataCollection(unittest.TestCase):
 
